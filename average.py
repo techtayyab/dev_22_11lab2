3f39e528bdbe887f5b4914d648bef3db03b20a89
techtayyab <ntayyab.tech@gmail.com>
2025-11-22 12:53:18 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,21 +1,18 @@
 import sys
 
-# Expecting 5 marks as arguments
 if len(sys.argv) < 6:
-    print("usage: python average.py <mark1> <mark2> <mark3> <mark4> <mark5>")
-    sys.exit(1)
-
-# Convert arguments to integers
-marks = [int(sys.argv[i]) for i in range(1, 6)]
-
-# Calculate average
-average = sum(marks) / 5
-
-print("Marks:", marks)
-print("Average:", average)
-
-# Grade logic
-if average >= 40:
-    print("You Passed.")
+    print("No marks provided, using defaults...")
+    marks = [10, 20, 30, 40, 50]
 else:
-    print("You Failed.")
+    marks = list(map(int, sys.argv[1:6]))
+
+avg = sum(marks) / len(marks)
+print("Average:", avg)
+
+# Fail if average is less than 40
+if avg < 40:
+    print("Build failed: average is below 40")
+    sys.exit(1)
+else:
+    print("Build passed")
+    sys.exit(0)
