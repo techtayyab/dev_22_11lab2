674a251b6830f9b8898c0ffbc7fe918a4ca65c96
techtayyab <ntayyab.tech@gmail.com>
2025-11-22 12:08:25 +0530

    Fix average calculation and grade assignment logic
    Corrected input handling and average calculation.

---
+++ average.py
@@ -0,0 +1,18 @@
+a = input ("Enter marks for subject 1: ")
+b = input ("Enter marks for subject 2: ")
+c = input ("Enter marks for subject 3: ")
+d = input ("Enter marks for subject 4: ")
+e = input ("Enter marks for subject 5: ")
+
+avg = a + b + c + d + e / 5
+
+print ("Average of 5 subjects: ", avg)
+
+if (avg > 80):
+  print ("Grade: A")
+elif (avg > 60 or avg < 80):
+  print ("Grade: B")
+elif (avg > 40 or avg < 60):
+  print ("Grade: C")
+else:
+  print ("You Failed.")
